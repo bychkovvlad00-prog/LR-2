1b158417458bf14ab744ed7552208a5df873db7e
Владислав <bychkovvlad00@gmail.com>
2025-12-06 19:04:00 +0300

    Add files via upload

---
+++ LR №2.1.py
@@ -0,0 +1,20 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+current_capital = money_capital
+months = 0
+
+while current_capital >= 0:
+    budget = salary + current_capital
+    if budget < spend:
+        break
+    current_capital -= (spend - salary)
+    months += 1
+    spend *= (1 + increase)
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
+
